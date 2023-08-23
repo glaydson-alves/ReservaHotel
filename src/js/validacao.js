e2011174bae209d4fb7d0d9ec48811c94a8078c5
Glaydson Alves <glaydson.alves36@gmail.com>
2023-08-22 21:01:18 -0300

    ...

---
+++ src/js/validacao.js
@@ -0,0 +1,29 @@
+// let entrada = document.querySelector("#entrada")
+// let validacao = document.querySelector("#validacao")
+
+// entrada.addEventListener("blur", ()=>{
+//     if(entrada.value == "dd/mm/aaaa"){
+//         validacao.classList.remove("d-none")
+//         validacao.classList.add("d-block")
+//     }
+//     else{
+//         validacao.classList.remove("d-block")
+//         validacao.classList.add("d-none")
+//     }
+// })
+
+
+
+// let crianca = document.querySelector("#crianca")
+// let span_crianca = document.querySelector("#span_crianca")
+
+// entrada.addEventListener("blur", ()=>{
+//     if(crianca.value == ""){
+//         span_crianca.classList.remove("d-none")
+//         span_crianca.classList.add("d-block")
+//     }
+//     else{
+//         span_crianca.classList.remove("d-block")
+//         span_crianca.classList.add("d-none")
+//     }
+// })
